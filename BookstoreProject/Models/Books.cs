ff6c296a80135ef03c6be4aa2de17162e137b79b
Denis Shirokov <115002482+Pentoxtar@users.noreply.github.com>
2023-05-24 13:45:36 +0300

    Add files via upload

---
+++ BookstoreProject/Models/Books.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookstoreProject.Models
+{
+    public class Books
+    {
+        public string Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Publisher { get; set; }
+        public string ISBN { get; set;}
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime DatePublished { get; set;}
+        public double Price { get; set; }
+        public string BookPictureURL { get; set;}
+    }
+}
