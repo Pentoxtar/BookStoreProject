154db93cc7fe98e751834ae70b98543d88694221
Denis Shirokov <115002482+Pentoxtar@users.noreply.github.com>
2023-05-25 12:06:00 +0300

    Add files via upload

---
+++ BookstoreProject/Controllers/BooksController.cs
@@ -0,0 +1,100 @@
+﻿using BookstoreProject.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreProject.Controllers
+{
+    public class BooksController : Controller
+    {
+		// GET: BooksController
+
+		//private readonly BooksService _booksService;
+
+		//public BooksController(BooksService booksService)
+		//{
+		//	_booksService = booksService;
+		//}
+
+		//public async Task<IActionResult> Index2()
+		//{
+		//	// Извикване на метода GetBooksFromGoogleBooksAPI от сервиза
+		//	var books = await _booksService.GetBooksFromGoogleBooksAPI();
+
+		//	return View(books);
+		//}
+		public ActionResult Index()
+        {
+            return View();
+        }
+        
+        
+        // GET: BooksController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BooksController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BooksController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BooksController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BooksController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BooksController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
