c1691a0804d922cbe597d2f31e3b835ee1c8dec8
Denis Shirokov <115002482+Pentoxtar@users.noreply.github.com>
2023-05-25 12:05:24 +0300

    Add files via upload

---
+++ BookstoreProject/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BookstoreProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public List<Books> BooksCheckedOut { get; set; }
+    }
+}
