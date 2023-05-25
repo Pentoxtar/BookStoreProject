3e98cc529c4ce6c36a782f9960fdfcdb797ddc1d
Denis Shirokov <115002482+Pentoxtar@users.noreply.github.com>
2023-05-25 12:19:11 +0300

    Add files via upload

---
+++ BookstoreProject/Services/BooksService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using BookstoreProject.Models;
+namespace BookstoreProject.Services
+{
+    public class BooksService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BooksService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<Books>> GetBooksFromGoogleBooksAPI()
+        {
+            var apiUrl = "https://www.googleapis.com/books/v1/volumes?q=programming";
+
+            
+            var response = await _httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+
+               
+                var books = JsonConvert.DeserializeObject<List<Books>>(json);
+
+                
+                return books ?? new List<Books>();
+            }
+
+            
+            return new List<Books>();
+        }
+    }
+}
