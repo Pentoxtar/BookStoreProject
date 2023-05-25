561f70ada488ee70229aaf6d606f9c164eda6935
Denis Shirokov <115002482+Pentoxtar@users.noreply.github.com>
2023-05-25 12:07:13 +0300

    Update Books.cs

--- BookstoreProject/Models/Books.cs
+++ BookstoreProject/Models/Books.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 
 namespace BookstoreProject.Models
@@ -6,14 +6,30 @@ namespace BookstoreProject.Models
     public class Books
     {
         public string Id { get; set; }
+
         [Required]
+        [StringLength(80)]
+        [Display(Name ="Title")]
         public string Title { get; set; }
+
+        [StringLength(50)]
+        [Required]
+		[Display(Name = "Author")]
+		public string Author { get; set; }
+
         public string Description { get; set; }
-        public string Publisher { get; set; }
-        public string ISBN { get; set;}
+		[Display(Name = "Publisher")]
+		public string Publisher { get; set; }
+        [Required]
+		[Display(Name = "ISBN")]
+		public string ISBN { get; set;}
+
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime DatePublished { get; set;}
+
+        [Required]
         public double Price { get; set; }
-        public string BookPictureURL { get; set;}
+		[Display(Name = "BookPictureURL")]
+		public string BookPictureURL { get; set;}
     }
 }
