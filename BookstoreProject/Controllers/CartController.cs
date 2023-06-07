0f99fc85813cf4ae6c93b5c48e8f10731fd0dfc1
Denis Shirokov <115002482+Pentoxtar@users.noreply.github.com>
2023-06-07 19:07:15 +0300

    Add files via upload

---
+++ BookstoreProject/Controllers/CartController.cs
@@ -0,0 +1,41 @@
+﻿using BookstoreProject.Models;
+using BookstoreProject.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BookstoreProject.Controllers
+{
+	public class CartController : Controller
+	{
+        private readonly ICartService _cartService;
+        
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+		public async Task<IActionResult> Index()
+		{
+            Cart cart = _cartService.ViewCart();
+			return View(cart);
+		}
+
+        public IActionResult AddToCart(string id)
+        {
+            _cartService.AddToCart(id);
+            return RedirectToAction("Index");
+        }
+		public IActionResult RemoveFromCart(string id)
+		{
+			_cartService.RemoveFromCart(id);
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult ClearCart()
+		{
+			_cartService.ClearCart();
+			return RedirectToAction("Index");
+		}
+
+	}
+}
