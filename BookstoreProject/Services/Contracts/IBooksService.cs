1e97e05e4e3302a636c47ac1fa4dc4fb3bcea0bb
Denis Shirokov <115002482+Pentoxtar@users.noreply.github.com>
2023-06-07 19:09:01 +0300

    Add files via upload

--- BookstoreProject/Services/Contracts/IBooksService.cs
+++ BookstoreProject/Services/Contracts/IBooksService.cs
@@ -4,7 +4,7 @@ namespace BookstoreProject.Services.Contracts
 {
     public interface IBooksService
     {
-        public Task<List<Book>> GetBooksFromGoogleBooksAPI();
-       
-    }
+		public Task<List<Book>> GetBooksFromGoogleBooksAPI();
+		Task<List<CartItem>> GetCartItemsById(string id);
+	}
 }
---
+++ BookstoreProject/Services/Contracts/ICartService.cs
@@ -0,0 +1,16 @@
+﻿using BookstoreProject.Models;
+
+namespace BookstoreProject.Services.Contracts
+{
+    public interface ICartService
+    {
+        public void AddToCart(string id);
+        public void RemoveFromCart(string id);
+        public void ClearCart();
+        public Cart GetCart();
+
+        public void SaveCart(Cart cart);
+
+        public Cart ViewCart();
+    }
+}
