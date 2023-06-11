18238b2265161a510899434542c9657d92232679
Denis Shirokov <115002482+Pentoxtar@users.noreply.github.com>
2023-06-11 14:23:08 +0300

    Add files via upload

--- BookstoreProject/wwwroot/js/site.js
+++ BookstoreProject/wwwroot/js/site.js
@@ -1,4 +1,48 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//Register functionality
+document.addEventListener("DOMContentLoaded", function () {
+    var passwordToggle = document.getElementById("showPasswordToggle");
+    var confirmPasswordToggle = document.getElementById("showConfirmPasswordToggle");
 
-// Write your JavaScript code.
+    passwordToggle.addEventListener("click", function () {
+        var passwordInput = document.getElementById(passwordToggle.dataset.target);
+        togglePasswordVisibility(passwordInput, passwordToggle);
+    });
+
+    confirmPasswordToggle.addEventListener("click", function () {
+        var confirmPasswordInput = document.getElementById(confirmPasswordToggle.dataset.target);
+        togglePasswordVisibility(confirmPasswordInput, confirmPasswordToggle);
+    });
+
+    function togglePasswordVisibility(input, toggle) {
+        if (input.type === "password") {
+            input.type = "text";
+            toggle.innerHTML = ' <i class="far fa-eye-slash"></i>Hide';
+        } else {
+            input.type = "password";
+            toggle.innerHTML = ' <i class="far fa-eye"></i>Show';
+        }
+    }
+});
+
+//Shopping cart Icon
+var cartIcon = document.getElementById("cart-icon");
+
+cartIcon.addEventListener("mouseenter", function () {
+    cartIcon.classList.add("fa-cart-shopping-animation");
+});
+
+cartIcon.addEventListener("mouseleave", function () {
+    cartIcon.classList.remove("fa-cart-shopping-animation");
+});
+
+
+//Account Icon
+var accountIcon = document.getElementById('accountIcon');
+
+accountIcon.addEventListener('mouseenter', function () {
+    accountIcon.classList.add('rotate-animation');
+});
+
+accountIcon.addEventListener('mouseleave', function () {
+    accountIcon.classList.remove('rotate-animation');
+});
