4606b5f6dcb6f4155a3e86cf9549d716c503fd78
Denis Shirokov <115002482+Pentoxtar@users.noreply.github.com>
2023-06-11 14:24:51 +0300

    Add files via upload

---
+++ BookstoreProject/ViewModel/AddRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookstoreProject.ViewModel
+{
+	public class AddRoleViewModel
+	{
+		[Required]
+		[Display(Name ="Role")]
+        public string RoleName { get; set; }
+    }
+}
